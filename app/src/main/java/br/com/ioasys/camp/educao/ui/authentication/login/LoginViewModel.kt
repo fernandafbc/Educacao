865ae37c1d0725e861036ad07dfff3207fe96901
raulkantor <raulsauaia@gmail.com>
2020-02-09 09:45:04 -0200

    AI-3.5.3 <raul@raulPC Overwrite remote https://github.com/fernandafbc/Educacao.git to local

--- app/src/main/java/br/com/ioasys/camp/educao/ui/authentication/login/LoginViewModel.kt
+++
@@ -1,7 +0,0 @@
-package br.com.ioasys.camp.educao.ui.authentication.login
-
-import androidx.lifecycle.ViewModel
-
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/br/com/ioasys/camp/educao/ui/main/Monitor.kt
+++
@@ -1,8 +0,0 @@
-package br.com.ioasys.camp.educao.ui.main
-
-data class Monitor (
-    val name: String,
-    val age: String,
-    val university: String
-    //val subjects: ArrayList<String>
-)
